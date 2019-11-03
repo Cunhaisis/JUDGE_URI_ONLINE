bfc842d81de35421d73baa5fb5cba9b061572e0c
Cunhaisis <36779905+Cunhaisis@users.noreply.github.com>
2019-11-03 19:56:25 -0300

    Update 1037.py

--- JUDGE URI ONLINE/1037.py
+++ JUDGE URI ONLINE/1037.py
@@ -1,3 +1,11 @@
 N = float(input())
-if N < 0 or N > 100:
+if N >= 0 and N <= 25.0000:
+    print('Intervalo [0,25]')
+elif N >= 25.00001 and N <= 50.0000000:
+    print('Intervalo (25,50]')
+elif N >= 50.00001 and N <= 75.0000000:
+    print('Intervalo (50,75]')
+elif N >= 75.00001 and N <= 100.0000000:
+    print('Intervalo (75,100]')
+else:
     print('Fora de intervalo')
